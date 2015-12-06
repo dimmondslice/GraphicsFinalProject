866d5810b9c789b2039caf0316cf04ef496c11f5
unknown <shahp2@SA2411-PC13.win.rpi.edu>
2015-12-06 03:33:53 -0500

    Added Some things for the de-mo also made a script to modify the cloaking values

--- GraphicsFinal/Assets/Scripts/Player/PlayerController.cs
+++ GraphicsFinal/Assets/Scripts/Player/PlayerController.cs
@@ -67,14 +67,14 @@ public class PlayerController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        //Cursor.lockState = CursorLockMode.Locked;
+       // Cursor.visible = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+       // Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.visible = false;
 
         SendInput();
 
