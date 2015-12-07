b9d9b42b11c2367b164934ed4f57b170ebbfbbfd
unknown <shahp2@SA2411-PC23.win.rpi.edu>
2015-12-06 21:33:24 -0500

    (g7)  added Splat to the Demo Note For Dom: Make Character move faster please.

---
+++ GraphicsFinal/Assets/Destroy.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroy : MonoBehaviour {
+    public float Timer = 5;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Timer -= Time.deltaTime;
+
+        if (Timer <= 0.0f)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
